fc3a55fac33ef4c408252b2dc92af3da420bfc0c
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-05-23 22:34:59 +1000

    Import functions from modules

--- episode04/user.exs
+++ episode04/user.exs
@@ -1,15 +1,18 @@
 defmodule User do
+  import String, only: [split: 1]
+  import List, only: [first: 1, last: 1]
+
   defstruct name: nil, email: nil
 
   def first_name(user) do
     user.name
-    |> String.split
-    |> List.first
+    |> split
+    |> first
   end
 
   def last_name(user) do
     user.name
-    |> String.split
-    |> List.last
+    |> split
+    |> last
   end
 end
