ddcdc3a8169d7b2959f53aa9a7ea6f01b771ea83
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-05-23 22:43:18 +1000

    Add documentation

--- episode04/user.exs
+++ episode04/user.exs
@@ -1,15 +1,45 @@
 defmodule User do
+  @moduledoc """
+  Defines the user struct and functions to handle users.
+  """
+
   import String, only: [split: 1]
   import List, only: [first: 1, last: 1]
 
   defstruct name: nil, email: nil
 
+  @doc """
+  Get the first name of a user
+
+  ## Parameters
+
+  - `user` - A User struct
+
+  ## Examples
+
+      user = %User{ name: "Alice Winston" }
+      User.first_name(user)
+      "Alice"
+  """
   def first_name(user) do
     user.name
     |> split
     |> first
   end
 
+  @doc """
+  Get the last name of a user
+
+  ## Parameters
+
+  - `user` - A User struct
+
+  ## Examples
+
+      user = %User{ name: "Alice Winston" }
+      User.last_name(user)
+      "Winston"
+  """
   def last_name(user) do
     user.name
     |> split
