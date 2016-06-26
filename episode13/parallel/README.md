b85076c09ea64a58f1d1f23a984d0935b4cc65e5
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-06-27 08:59:49 +1000

    Complete episode 13

---
+++ episode13/parallel/README.md
@@ -0,0 +1,24 @@
+# Parallel
+
+**TODO: Add description**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed as:
+
+  1. Add `parallel` to your list of dependencies in `mix.exs`:
+
+    ```elixir
+    def deps do
+      [{:parallel, "~> 0.1.0"}]
+    end
+    ```
+
+  2. Ensure `parallel` is started before your application:
+
+    ```elixir
+    def application do
+      [applications: [:parallel]]
+    end
+    ```
+
---
+++ episode13/parallel/lib/parallel.ex
@@ -0,0 +1,36 @@
+defmodule Parallel do
+  @moduledoc"""
+  Example module demonstrating parallel processes from Learn Elixir
+  Episode 13
+  """
+
+  @doc"""
+  Parallel map function that takes in a `collection` and `fun`
+  function and performs a function call on each member of the
+  collection in a separate process.
+
+  Returns a collection of values
+
+  Examples:
+
+      iex> Parallel.pmap(1..10, &(&1 * &1))
+      [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+  """
+  def pmap(collection, fun) do
+    collection
+    |> Enum.map(&spawn_process(&1, self, fun))
+    |> Enum.map(&await/1)
+  end
+
+  defp spawn_process(item, parent, fun) do
+    spawn_link fn ->
+      send parent, { self, fun.(item) }
+    end
+  end
+
+  defp await(pid) do
+    receive do
+      { ^pid, result } -> result
+    end
+  end
+end
