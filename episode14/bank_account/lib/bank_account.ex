fc9fb6178bd6028989210f1fab78e9901ad012f3
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-07-09 16:38:26 +0930

    Finish bank account code for episode 14

--- episode14/bank_account/lib/bank_account.ex
+++ episode14/bank_account/lib/bank_account.ex
@@ -1,19 +1,65 @@
 defmodule BankAccount do
-  use GenServer
+  # NOTE: Commented out code refers to version of BankAccount
+  # that uses GenServer explicitly in calls ie:
+  # iex> { :ok, account } = GenServer.start(BankAccount, 0)
+  # ...> GenServer.cast(account, { :deposit, 50 })
+  # ...> GenServer.cast(account, { :withdrawal, 25 })
+  # ...> balance = GenServer.call(account, :balance)
+  # => 25
 
-  def init(balance) do
-    { :ok, balance }
+  # use GenServer
+
+  # def init(balance) do
+  #   { :ok, balance }
+  # end
+
+  # def handle_cast({ :deposit, amount }, balance) do
+  #   { :noreply, balance + amount }
+  # end
+
+  # def handle_cast({ :withdrawal, amount }, balance) do
+  #   { :noreply, balance - amount }
+  # end
+
+  # def handle_call(:balance, _from, balance) do
+  #   { :reply, balance, balance }
+  # end
+
+  # NOTE: The following commented out code for BankAccount is just
+  # a different implementation using a struct
+
+  # defstruct balance: 0
+
+  # def new(balance) do
+  #   %__MODULE__{ balance: balance }
+  # end
+
+  # def deposit(account, amount) do
+  #   %{ account | balance: account.balance + amount }
+  # end
+
+  # def deposit(account, amount) do
+  #   %{ account | balance: account.balance - amount }
+  # end
+
+  # def balance(account) do
+  #   account.balance
+  # end
+
+  def start(balance) do
+    { :ok, account } = GenServer.start(__MODULE__, balance)
+    account
   end
 
-  def handle_cast({ :deposit, amount }, balance) do
-    { :noreply, balance + amount }
+  def deposit(account, amount) do
+    GenServer.cast(account, { :deposit, amount })
   end
 
-  def handle_cast({ :withdrawal, amount }, balance) do
-    { :noreply, balance - amount }
+  def withdraw(account, amount) do
+    GenServer.cast(account, { :withdraw, amount })
   end
 
-  def handle_call(:balance, _from, balance) do
-    { :reply, balance, balance }
+  def balance(account) do
+    GenServer.call(account, :balance)
   end
 end
