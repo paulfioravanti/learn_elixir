d7e0de4f2f1f2716521b8fa32a28f587677be0c6
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-08-24 08:58:31 +1000

    Fix typo

--- episode16/todo/test/todo/server_test.exs
+++ episode16/todo/test/todo/server_test.exs
@@ -3,13 +3,13 @@ defmodule Todo.ServerTest do
 
   alias Todo.Server
 
-  # setup do
-  #   on_exit fn ->
-  #     Enum.each Server.lists fn(list) ->
-  #       Server.delete_list(list)
-  #     end
-  #   end
-  # end
+  setup do
+    on_exit fn ->
+      Enum.each Server.lists, fn(list) ->
+        Server.delete_list(list)
+      end
+    end
+  end
 
   test ".add_list adds a new supervised todo list" do
     Server.add_list("Home")
@@ -26,13 +26,13 @@ defmodule Todo.ServerTest do
     assert is_pid(list)
   end
 
-  # test ".delete_list deletes a list by its name" do
-  #   Server.add_list("delete-me")
-  #   list = Server.find_list("delete-me")
+  test ".delete_list deletes a list by its name" do
+    Server.add_list("delete-me")
+    list = Server.find_list("delete-me")
 
-  #   Server.delete_list(list)
-  #   counts = Supervisor.count_children(Server)
+    Server.delete_list(list)
+    counts = Supervisor.count_children(Server)
 
-  #   assert counts.active == 0
-  # end
+    assert counts.active == 0
+  end
 end
